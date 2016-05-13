6f72d36fa404fb10d47f94e99fcca61481d4f566
Hussen Fakhrulddin <hf556482@gmail.com>
2016-05-12 21:59:49 -0700

    init'

---
+++ LINQ-VAR/LINQ-VAR/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_VAR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Arr = { "Hussen", "Fakhrulddin" };
+
+            var o = from x in Arr where x.Length > 5 select new { Len = x.Length, Value = x };
+            foreach (var x in o)
+            {
+                Console.WriteLine(x.Len + " " + x.Value);
+            }
+            Console.ReadLine();
+        }
+    }
+}
